aabfab18b6e4a59bcbd0f15bb83b33bc743e4942
hetncr <97675352+hetncr@users.noreply.github.com>
2024-01-25 15:53:40 +0300

    Update tuncerhissehesaplayici.py

--- tuncerhissehesaplayici.py
+++ tuncerhissehesaplayici.py
@@ -200,7 +200,7 @@ if hisse_adi:
   if stock_name in hisse_oran:
       try:
           # Access the stock data and extract the F/K value
-          kapanıs = hisse_oran[stock_name]["kapanıs"].replace(",", ".")
+          kapanıs = hisse_oran[stock_name]["kapanıs"] #.replace(",", ".")
           fk_value = hisse_oran[stock_name]["f_k"].replace(",", ".")  # Format with dots as decimal separators
           pd_value = hisse_oran[stock_name]["pd_dd"].replace(",", ".")
           st.write(f"   :chart:**:blue[HİSSE FİYATI:]**  {kapanıs}") #, box = True)
