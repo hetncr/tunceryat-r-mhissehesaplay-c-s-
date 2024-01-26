8995c22fb45a0d910f8dbab088226053d4ac6424
hetncr <97675352+hetncr@users.noreply.github.com>
2024-01-27 01:54:58 +0300

    Update tuncerhissehesaplayici.py

--- tuncerhissehesaplayici.py
+++ tuncerhissehesaplayici.py
@@ -10,7 +10,7 @@ import pandas as pd
 
 
 #streamlit.config.theme.base = "dark"
-st.title("**HİSSE FİYAT HESAPLAYICISI**")
+st.title(":blue[**Hisse Hesap Uygulaması**]")
 st.subheader(":chart:**TUNCER YATIRIM** :chart:", divider='rainbow')
 #st.set_page_config(
 # page_title="Hisse Hedef Fiyat Hesaplayıcı",
