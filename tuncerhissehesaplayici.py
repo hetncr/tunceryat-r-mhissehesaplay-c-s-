5be7c5e4296a4151c5c87a8cbcea57a256de577b
hetncr <97675352+hetncr@users.noreply.github.com>
2024-02-18 02:13:10 +0300

    Update tuncerhissehesaplayici.py

--- tuncerhissehesaplayici.py
+++ tuncerhissehesaplayici.py
@@ -318,7 +318,8 @@ if hisse_adi:
       try:
           # Access the stock data and extract the F/K value
           kapanıs = hisse_oran[stock_name]["kapanıs"]  # Extract kapanıs 
-          kapanıs = int(kapanıs.replace(",",".").replace(",","."))
+          kapanıs = int(kapanıs)
+          kapanıs = f"{kapanıs:,.2f}"
           ##kapanıs = float(kapanıs.replace(",","").replace(",",""))
           st.write(f"   :chart:**:blue[HİSSE FİYATI:]**  {kapanıs}") #, box = True)     
           #else:
