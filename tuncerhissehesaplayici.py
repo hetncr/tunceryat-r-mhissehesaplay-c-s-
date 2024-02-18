2a95c0ac5d7cf2cb8d25f604840596905aa08443
hetncr <97675352+hetncr@users.noreply.github.com>
2024-02-18 03:12:39 +0300

    Update tuncerhissehesaplayici.py

--- tuncerhissehesaplayici.py
+++ tuncerhissehesaplayici.py
@@ -367,7 +367,7 @@ if hisse_adi:
   ##c3 = float(kapanıs).replace(",", "."))
   #piyasa_degeri = kapanıs*OdenmisSermaye1
   #kapanıs = piyasa_degeri/OdenmisSermaye1
-  c3 = float(kapanıs.replace(",","."))
+  c3 = float(kapanıs)
   #c3 = float(c3.replace(",",""))
   ##kapanıs_1 = float(kapanıs.replace(",", "."))
   ##c3 = float({kapanıs_1:,.2f})
