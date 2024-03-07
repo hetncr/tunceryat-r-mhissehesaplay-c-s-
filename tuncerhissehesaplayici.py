16176826ce85b739c388ada7fae010fcf008acc2
hetncr <97675352+hetncr@users.noreply.github.com>
2024-03-07 22:13:22 +0300

    Update tuncerhissehesaplayici.py

--- tuncerhissehesaplayici.py
+++ tuncerhissehesaplayici.py
@@ -28,7 +28,7 @@ hisse_adi = hisse_input
 tdv = "https://tr.tradingview.com/symbols/BIST-"+hisse_adi
 isyatırım ="https://www.isyatirim.com.tr/tr-tr/analiz/hisse/Sayfalar/sirket-karti.aspx?hisse="+hisse_adi
 #st.write(" İş Yatırım Sayfası İçin Tıklayın: ") #[link](https://www.isyatirim.com.tr/tr-tr/analiz/hisse/Sayfalar/default.aspx)")
-st.link_button("İş Yatırım",isyatırım)("Tradingview", tdv)
+st.link_button("İş Yatırım",isyatırım),("Tradingview", tdv)
 st.link_button("Tradingview", tdv)
 
 if hisse_adi:
