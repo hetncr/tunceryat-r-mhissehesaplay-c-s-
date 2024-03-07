23b4304f7a986e1376ab2467a180019b98bf4746
hetncr <97675352+hetncr@users.noreply.github.com>
2024-03-07 21:12:57 +0300

    Update tuncerhissehesaplayici.py

--- tuncerhissehesaplayici.py
+++ tuncerhissehesaplayici.py
@@ -133,7 +133,7 @@ if hisse_adi:
     # skip the iteration
     #continue
   
-  tdv = "https://tr.tradingview.com/symbols/BIST-"+hisse_adi
+  tdv = https://tr.tradingview.com/symbols/BIST-hisse_adi
   st.write(" İş Yatırım Sayfası İçin Tıklayın: [link](https://www.isyatirim.com.tr/tr-tr/analiz/hisse/Sayfalar/default.aspx)")
   st.write(" Tradingview Grafik Sayfası İçin Tıklayın: "(tdv))
   #(https://tr.tradingview.com/symbols/BIST-hisse_input)")  
